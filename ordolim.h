289ea5dbad007befedcf9f36408117a0391e2d2b
Miguel A. Ballicora <mballic@luc.edu>
2012-01-13 00:02:18 -0600

    [v0.2.8-pgn20] include ordolim.h

---
+++ ordolim.h
@@ -0,0 +1,12 @@
+#if !defined(H_OLIM)
+#define H_OLIM
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+
+#define MAXGAMES 1000000
+#define LABELBUFFERSIZE 100000
+#define MAXPLAYERS 10000
+
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+#endif
