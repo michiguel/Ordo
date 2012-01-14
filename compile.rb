e4ee3ad48c5a10db28b844039e1bf5eade35c115
Miguel A. Ballicora <mballic@luc.edu>
2012-01-13 22:10:39 -0600

    [v0.3-comp1] change compiler directives

--- compile.rb
+++ compile.rb
@@ -7,6 +7,20 @@ if ARGV[0] == 'gcc'
 	$CC='gcc'
 	$NAME='ordo'
 
+elsif ARGV[0] == 'gcc64'
+
+	puts 'gcc selected'
+	$ADDPATH=''
+	$CC='gcc'
+	$NAME='ordo-linux64'
+
+elsif ARGV[0] == 'gcc32'
+
+	puts 'gcc32 selected'
+	$ADDPATH=''
+	$CC='gcc -m32'
+	$NAME='ordo-linux32'
+
 elsif ARGV[0] == 'win32'  
 
 	puts 'win32 selected'
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "v0.3"
+#define VERSION "v0.3-comp1"
 
