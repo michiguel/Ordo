d8cff77aeb5ac12b0d1ebaaec4a1a382dd20d9ab
Miguel A. Ballicora <mballic@luc.edu>
2012-09-26 21:08:07 -0500

    [v0.5.1-out7-cln2-csv1-dsim4-nspp10-conn20-gaus2] cleanup and add files

---
+++ gauss.c
@@ -0,0 +1,69 @@
+#include <math.h>
+#include "gauss.h"
+
+#define M_PI 3.14159265358979323846
+
+//static Gauss_A = 1.0 / sqrt(2 * M_PI);
+
+double gauss_density(double x) {return (1.0 / sqrt(2 * M_PI)) * exp(-0.5*x*x);}
+
+double gauss_integral(double z)
+{
+//http://finance.bi.no/~bernt/gcc_prog/recipes/recipes/node23.html
+	double b1;
+	double b2;
+	double b3;	
+	double b4;
+	double b5;
+	double p ;
+	double c2;
+	
+	double a; 
+	double t; 
+	double b; 
+	double n; 	
+
+	if (z >  6.0) return 1.0;
+	if (z < -6.0) return 0.0;
+
+	b1 = 0.31938153;
+	b2 = -0.356563782;
+	b3 = 1.781477937;	
+	b4 = -1.821255978;
+	b5 = 1.330274429;
+	p  = 0.2316419;
+	c2 = 0.3989423;
+	
+	a  = fabs(z);
+	t = 1.0/(1.0+a*p);
+	b = c2*exp((-z)*(z/2.0));
+	n = ((((b5*t+b4)*t+b3)*t+b2)*t+b1)*t;	
+	n = 1.0-b*n;
+	if (z < 0.0) n = 1.0-n;
+	return n;
+}
+
+static double symrange(double z) {return 2.0 * (gauss_integral(z) - 0.5);}
+
+double confidence2x (double confidence)
+{
+	double i,j,k,rj;
+	int x;
+
+	i = 0;
+	j = 3;
+	k = 6;
+	
+	for (x = 0; x < 32; x++) {
+		rj = symrange(j);
+		if (confidence > rj) {
+			i = j; 
+		} else {
+			k = j; 
+		}
+		j = (i+k)/2;
+	}
+	return j;
+}
+
+
---
+++ gauss.h
@@ -0,0 +1,10 @@
+#if !defined(H_GAUSS)
+#define H_GAUSS
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+double gauss_density (double x); 
+double gauss_integral(double z);
+double confidence2x (double confidence);
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+#endif
