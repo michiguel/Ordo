4c5b10885aa470161b8129f0149c15896c555df5
Miguel A. Ballicora <mballic@luc.edu>
2014-08-09 19:34:58 -0500

    [v0.8.5-cln44] reorganized

--- main.c
+++ main.c
@@ -2049,6 +2049,60 @@ adjust_wadv_bayes
 }
 
 
+static double
+fxpect (double a, double b, double beta)
+{
+	return 1.0 / (1.0 + exp((b-a)*beta));
+}
+
+#define DRAWRATE_AT_EQUAL_STRENGTH 0.33
+#define DRAWFACTOR (1/(2*(DRAWRATE_AT_EQUAL_STRENGTH))-0.5)
+
+static void
+fget_pWDL(double dr /*delta rating*/, double *pw, double *pd, double *pl, double beta)
+{
+	// Performance comprises wins and draws.
+	// if f is expected performance from 0 to 1.0, then
+	// f = pwin + pdraw/2
+	// from that, dc is the fraction of points that come from draws, not wins, so
+	// pdraw (probability of draw) = 2 * f * dc
+	// calculation of dc is an empirical formula to fit average data from CCRL:
+	// Draw rate of equal engines is near 0.33, and decays on uneven matches.
+
+	double f;
+	double pdra, pwin, plos;
+// 	double dc; 
+	bool_t switched;
+	
+	switched = dr < 0;
+
+	if (switched) dr = -dr;
+
+	f = fxpect (dr,0,beta);
+
+#if 0
+	dc = 0.5 / (0.5 + DRAWFACTOR * exp(dr*beta));
+	pdra = 2 * f * dc;
+	pwin = f - pdra/2;
+	plos = 1 - pwin - pdra; 
+#else
+	pwin = f * f;
+	plos = (1-f) * (1-f);
+	pdra = 1 - pwin - plos;
+#endif
+
+	if (switched) {
+		*pw = plos;
+		*pd = pdra;
+		*pl = pwin;
+	} else {
+		*pw = pwin;
+		*pd = pdra;
+		*pl = plos;
+	}
+	return;
+}
+
 static void fget_pWDL(double dr /*delta rating*/, double *pw, double *pd, double *pl, double beta);
 
 static double
@@ -2358,6 +2412,7 @@ adjust_rating_bayes
 	return ymax * delta;
 }
 
+//mojon
 
 
 static void
@@ -2604,63 +2659,6 @@ rand_threeway_wscore(double pwin, double pdraw)
 	}
 }
 
-
-#define DRAWRATE_AT_EQUAL_STRENGTH 0.33
-#define DRAWFACTOR (1/(2*(DRAWRATE_AT_EQUAL_STRENGTH))-0.5)
-
-static double
-fxpect (double a, double b, double beta)
-{
-	return 1.0 / (1.0 + exp((b-a)*beta));
-}
-
-#define DRAWRATE_AT_EQUAL_STRENGTH 0.33
-#define DRAWFACTOR (1/(2*(DRAWRATE_AT_EQUAL_STRENGTH))-0.5)
-
-static void
-fget_pWDL(double dr /*delta rating*/, double *pw, double *pd, double *pl, double beta)
-{
-	// Performance comprises wins and draws.
-	// if f is expected performance from 0 to 1.0, then
-	// f = pwin + pdraw/2
-	// from that, dc is the fraction of points that come from draws, not wins, so
-	// pdraw (probability of draw) = 2 * f * dc
-	// calculation of dc is an empirical formula to fit average data from CCRL:
-	// Draw rate of equal engines is near 0.33, and decays on uneven matches.
-
-	double f;
-	double pdra, pwin, plos;
-// 	double dc; 
-	bool_t switched;
-	
-	switched = dr < 0;
-
-	if (switched) dr = -dr;
-
-	f = fxpect (dr,0,beta);
-
-#if 0
-	dc = 0.5 / (0.5 + DRAWFACTOR * exp(dr*beta));
-	pdra = 2 * f * dc;
-	pwin = f - pdra/2;
-	plos = 1 - pwin - pdra; 
-#else
-	pwin = f * f;
-	plos = (1-f) * (1-f);
-	pdra = 1 - pwin - plos;
-#endif
-
-	if (switched) {
-		*pw = plos;
-		*pd = pdra;
-		*pl = pwin;
-	} else {
-		*pw = pwin;
-		*pd = pdra;
-		*pl = plos;
-	}
-	return;
-}
 
 
 static void
